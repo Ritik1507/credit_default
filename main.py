a9a388affa5fa4d73407028d8bac3e2a7d645a13
ritik1507 <112415038+Ritik1507@users.noreply.github.com>
2024-08-10 20:11:24 +0530

    data validation complete

---
+++ main.py
@@ -0,0 +1,50 @@
+from credit_default import logger
+
+from credit_default.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from credit_default.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from credit_default.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from credit_default.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e  
+
+
+STAGE_NAME =  "Data Validation Stage"
+try:
+  logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+  obj = DataValidationTrainingPipeline()
+  obj.main()
+  logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage "
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
